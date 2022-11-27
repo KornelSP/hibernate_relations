5e56a99a17f8a1e2f8f278f2859655d659cd4c75
Kornel <kornelkrzyzan@gmail.com>
2022-11-27 13:43:39 +0100

    Commit po dlugiej przerwie

---
+++ src/main/java/pl/sda/hibernate/Create_InsertOcena.java
@@ -0,0 +1,44 @@
+package pl.sda.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.Scanner;
+
+public class Create_Insert {
+    public static void main() {
+        Scanner scanner = new Scanner(System.in);
+        // wywołaj try-with-resources który zamknie sesję automatycznie po opuszczeniu try
+        try (Session session = HibernateUtil.INSTANCE.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+
+            // Tworzymy studenta
+            System.out.println("Podaj imię studenta:");
+            String imie = scanner.nextLine();
+
+            System.out.println("Podaj nazwisko studenta:");
+            String nazwisko = scanner.nextLine();
+
+            System.out.println("Podaj rok rozpoczęcia studiów:");
+            String rokStudiow = scanner.nextLine();
+            int rok = Integer.parseInt(rokStudiow);
+
+
+            Student uczen = Student.builder()
+                    .imie(imie)
+                    .nazwisko(nazwisko)
+                    .rokRozpoczeciaStudiow(rok)
+                    .build();
+
+            // zapisujemy studenta
+            session.persist(uczen);
+
+            // zatwierdzamy transakcję
+            transaction.commit();
+        } catch (Exception ioe) {
+            // jeśli złapiemy błąd, to wywoła się catch
+            System.err.println("Błąd bazy: " + ioe);
+        }
+    }
+
+}
---
+++ src/main/java/pl/sda/hibernate/Student.java
@@ -0,0 +1,2 @@
+package pl.sda.hibernate;public class Student {
+}
